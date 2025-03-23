3cd5ef44cccb01afaa96b546e5aa8dfd16bca1b5
luiznwz <luizrenan179@gmail.com>
2025-03-23 00:37:39 -0300

    feat: header

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Eth from "../eth.svg";
+import { Link } from "react-router-dom";
 
-function Header() {
+export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="leftH">
+        <img src="{Eth}" alt="logo" className="logo" />
+        <Link to="/" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton">Connect</div>
+      </div>
+    </header>
+  );
 }
-
-export default Header
